c7a424d4933a799a345dc696cfa5e9c8bd45024f
Szalovszky Dávid <29980128+szalovszky@users.noreply.github.com>
2022-05-12 20:37:10 +0200

    Bugfixes, new source, better output
    - Fixed `Shazam` not checking & not deleting the files in the correct order
    - Added `Startpage_Deezer` as a source
    - Added `DuckDuckGo_Deezer` as an experimental source
    - Now filtering unicode out of title
    - `Shazam` is now faster
    - More information in `output.html`

--- platforms.py
+++ platforms.py
@@ -1,10 +1,16 @@
 import traceback
 import time
+import re
 import deezer
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+import random
 
 import constants
 from utils import utils
 import platforms
+import settings
 
 class deezer_platform:
     deezer_client = deezer.Client()
@@ -56,6 +62,23 @@ class deezer_platform:
                     break
         return res
 
+    def tracklink(url):
+        success = False
+        while(not success):
+            try:
+                # I know this can be done fully by RegEx, but I can't into RegEx
+                id = re.sub(r'(?<=https://www.deezer.com/).*(?=/track/)', '', url).replace("https://www.deezer.com//track/", "")
+                res = platforms.deezer_platform.deezer_client.request("GET", "track/" + id, resource_type=deezer.Track)
+                success = True
+            except Exception as e:
+                if("quota" in str(e).lower()):
+                    time.sleep(1)
+                else:
+                    print(traceback.format_exc())
+                    res = None
+                    break
+        return res
+
     def search_album(query):
         success = False
         while(not success):
@@ -86,7 +109,6 @@ class deezer_platform:
     def check_yt_res(original, result, query, search_ranking = False, featured_artists = False):
         if(result == None):
             return None
-        
         seperate = isinstance(query, list)
 
         try:
@@ -178,3 +200,128 @@ class deezer_platform:
                     else:
                         print(traceback.format_exc())
                         return None
+
+class ddg_platform:
+    class result:
+        def __init__(self, title, link):
+            self.title = title
+            self.link = link
+        def as_dict(self):
+            return self.__dict__
+
+    def search(query):
+        success = False
+        delay = 2
+        while(not success):
+            try:
+                headers = {
+                    'User-Agent': random.choice(constants.user_agents)
+                }
+
+                page = requests.get(f"https://html.duckduckgo.com/html/?q={query}", headers=headers)
+                res = []
+                if("If this error persists, please let us know" in str(page.content)):
+                    delay *= 2
+                    print(f"You are being ratelimited, waiting {delay} seconds before retrying...")
+                    if(delay >= 30):
+                        success = True
+                        res = False
+                    sleep(delay)
+                    continue
+                soup = BeautifulSoup(page.content, "html.parser")
+                elems = soup.find_all("div", class_="result__body")
+
+                for elem in elems:
+                    title = elem.find("a", class_="result__a")
+                    if(title == None):
+                        continue
+                    else:
+                        title = title.text
+                    link = "https://" + " ".join(elem.find("a", class_="result__url").text.split())
+                    res.append(ddg_platform.result(title, link).as_dict())
+
+                success = True
+                return res
+            except Exception:
+                print(traceback.format_exc())
+                return None
+
+    def search_track(query, use_spotify = False):
+        if(len(query) <= 4):
+            return None
+
+        try:
+            res = None
+            srch = ddg_platform.search(f"{query} site:{'deezer.com' if (not use_spotify) else 'open.spotify.com'}")
+            for srch_res in srch:
+                if((not use_spotify and srch_res['link'].startswith("https://www.deezer.com/") and ("/artist/" not in str(srch_res['link'])) and ("/playlist/" not in str(srch_res['link'])) and ("/album/" not in str(srch_res['link']))) or
+                (use_spotify and srch_res['link'].startswith("https://open.spotify.com"))):
+                    res = srch_res['link']
+                    # Remove non-numeric chars
+                    res = re.sub("[^0-9]", "", res)
+                    if(res == ""):
+                        res = None
+                    if(res != None):
+                        return res
+
+            return res
+        except Exception as e:
+            print(e)
+            return None
+
+class startpage_platform:
+    class result:
+        def __init__(self, title, link):
+            self.title = title
+            self.link = link
+        def as_dict(self):
+            return self.__dict__
+    
+    def search(query):
+        success = False
+        while(not success):
+            # An extra precaution to not get ratelimited
+            sleep(1)
+            try:
+                headers = {
+                    'User-Agent': random.choice(constants.user_agents)
+                }
+
+                page = requests.get(f"https://www.startpage.com/sp/search?query={query}", headers=headers)
+                res = []
+                soup = BeautifulSoup(page.content, "html.parser")
+                elems = soup.find_all("div", class_="w-gl__result__main")
+
+                for elem in elems:
+                    title = elem.find("h3").text
+                    link = elem.find("a", class_="result-link")['href']
+                    res.append(startpage_platform.result(title, link).as_dict())
+
+                success = True
+                return res
+            except Exception as e:
+                print(e)
+                return None
+
+    def search_track(query, use_spotify = False):
+        if(len(query) <= 4):
+            return None
+
+        try:
+            res = None
+            srch = startpage_platform.search(f"{query} site:{'deezer.com' if (not use_spotify) else 'open.spotify.com'}")
+            for srch_res in srch:
+                if((not use_spotify and srch_res['link'].startswith("https://www.deezer.com/") and ("/artist/" not in str(srch_res['link'])) and ("/playlist/" not in str(srch_res['link'])) and ("/album/" not in str(srch_res['link']))) or
+                (use_spotify and srch_res['link'].startswith("https://open.spotify.com"))):
+                    res = srch_res['link']
+                    # Remove non-numeric chars
+                    res = re.sub("[^0-9]", "", res)
+                    if(res == ""):
+                        res = None
+                    if(res != None):
+                        return res
+
+            return res
+        except Exception as e:
+            print(e)
+            return None
